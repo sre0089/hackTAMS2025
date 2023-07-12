5c32ecc4027d312f10d6dc41e61e062889f46a9f
seepiii <93924225+seepiii@users.noreply.github.com>
2023-07-12 12:06:18 -0500

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-export const REGISTRATION_LINK = "https://forms.gle/3Xpfj4DQVMLgQ7rZ7";
-export const DISCORD_LINK = "https://discord.gg/fVKDT6Ra5s";
+export const REGISTRATION_LINK = "https://forms.gle/BV6k97a5FQqgyyxn9";
+export const DISCORD_LINK = "https://discord.gg/N2yeVfpC";
 export const INSTAGRAM_LINK = "https://www.instagram.com/hacktams/";
 export const MLH_CODE_OF_CONDUCT_LINK = "https://static.mlh.io/docs/mlh-code-of-conduct.pdf";
 export const DONATION_LINK = "https://secure.givelively.org/donate/hackplus/hacktams";
