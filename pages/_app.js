6366705e0c6309f0eac232b1a1cdd85a505e0f33
sre0089 <138663463+sre0089@users.noreply.github.com>
2024-11-26 15:11:02 -0600

    Update _app.js
    edit to hackTAMS 2025

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>hackTAMS 2024</title>
+        <title>hackTAMS 2025</title>
         <meta name="description" content="hackTAMS is the annual hackathon organized by students at the Texas Academy of Mathematics and Science for students in the Dallas-Fort Worth area."/>
         <meta property="og:url" content="https://2024.hacktams.org"/>
         <meta property="og:title" content="hackTAMS 2024"/>
